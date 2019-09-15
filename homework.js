c7e814192ea841a533e0cfa7a3d32ec679d290e8
Natalia Walendzik <natalia.walendzik@gmail.com>
2019-09-15 13:41:29 +0200

    insert boxes work

--- homework.js
+++ homework.js
@@ -1,29 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    
 
-    function insertBoxes(products){
+    $(function () {
+        
+        var api = 'https://api.predic8.de/shop/products/';
 
-        var api = 'https://api.predic8.de/shop/products/';        
+        function insertBoxes(products) {
 
-        $.ajax({
-            url: api,
-            dataType: 'json',
-        }).done(function(res){
-            // Get one name from fixed list randomly
-            var name = res.products[Math.floor(Math.random() * 10)].name;
-            var container = $('.inputBoxes');
-            var listLetters = name.split('');
+            $.ajax({
+                url: api,
+                dataType: 'json',
+            }).done(function (res) {
+                
+                var name = res.products[Math.floor(Math.random() * 10)].name;
+                console.log(name);
+                var container = $('.inputBoxes');
+                var listLetters = name.split('');
+                console.log(listLetters);
 
-            listLetters.forEach(char => {return container.append($('<input>'))});
-            
-            return container;
+                listLetters.forEach(char => { return container.append($('<input type="text" pattern="[A-Za-z]{1}">')) });
+                return container;
 
-        }).fail(function(err){
-            console.log('Cannot connect with the server');
-        });
-        }
+            }).fail(function (err) {
+                console.log('Cannot connect with the server');
+            });
+        };
 
         insertBoxes();
+    });
 
-    })
-
- 
+})
