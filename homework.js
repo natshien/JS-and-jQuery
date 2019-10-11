35a4aed7ef61d23e624018245776f656a02e9811
Natalia Walendzik <41785900+natshien@users.noreply.github.com>
2019-10-11 15:32:13 +0200

    refactoring code in progress

--- homework.js
+++ homework.js
@@ -1,135 +1,130 @@
-$(document).ready(function(){
-const words = ["apple", "strawberry", "cherry", "mango"];
-
-
-function initialize(res){
-    var listWords = res && res.products && res.products.length && res.products.map(prod => prod.name) || words; 
-    var word = listWords[Math.floor(Math.random() * listWords.length)].toLowerCase();
-    console.log(word);
-    var form = $('.inputBoxes');
-    var listLetters = word.split('');
-    console.log(listLetters);
-    var userWord = [];
+x$(document).ready(function(){
     
-    // create inputs  + disabled space
-    listLetters.forEach((char, i) => {
-        if(listLetters[i] === " "){
-            return form.append($('<input disabled="true" value=" " id='+i+'>'))
-        } else {
-            return form.append($('<input id='+i+'>'))
-        }
-    });
-    
-    // allow only letters
-    $("input").on("keyup", checkInput);
-
-    function checkInput(event) {
-        var i = event.target.id;
-        var regex = /^[A-Za-z]{1}$/;
-        if(!$(event.target).val().match(regex) ) {
-            $(event.target).val("");
-        }
-        var input = event.target;
-        var value = input.value;
-        var triesLeft = $(".triesLeft");
-        var chances = parseInt(triesLeft.text());
-        
-        //var winner = userWord.concat();
-        if(!value) {
-            return;
-        }
-        if(value == listLetters[i]) {
-            var userWord = [];
-            console.log(value);
-            input.style.color = "green";
-            var id = "#" + input.id;
-            $(id).after("<div id='new'>"+value+"</div>");
-            userWord.push(value);
-            userWord.join("");
-        } else if(listLetters.indexOf(value) >= 0){
-            input.style.color = "orange";
-            triesLeft.text(chances-1);
-            if (triesLeft.text() <= 0){
-                $(".result").text("GAME OVER. TRY AGAIN!");
-                triesLeft.text('0');}
-        } else {
-            input.style.color = "red";
-            triesLeft.text(chances-1);
-            if (triesLeft.text() <= 0) {
-                $(".result").text("GAME OVER. TRY AGAIN !").css("color", "red");
-                triesLeft.text('0');
-            }                   
-        }
-        
-    };
-
-    function checkWin() {
-        var hints = $(".hints");
-
-        // lost
-        if (triesLeft.text() == 0) {
-            hints.attr('disabled', true); // !!!!!!!!!!!!!!! WWWWHYYYYYYYYY ??????????????????
-        }
-
-        var inputsTxt = document.getElementsByTagName("input").value;
-        userWord.push(inputsTxt);
-        var checkWord = userWord.join("");
-        
-
-
-    }
+    const wordsBackup = ["apple", "strawberry", "banana", "cherry", "mango"];
+    let maxTries = 9;
+    let maxHints = 3;
+    let currentWord;
+    let userWord = [];
+    let splitLetters;
     
 
-    // subtract the hints
-    $(".hints").on("click", function(event){
-        var hintsLeft = $(".hintsLeft");
-        var num = parseInt(hintsLeft.text());
-        var input = event.target;
-    
-        
-        hintsLeft.text(num-1);
-        if (hintsLeft.text() <= 0) {
-            hintsLeft.text('0');
-        }
-    })
-    
-    
-    //$(".result").text("CONGRATS !").css("color", "green");
-
-    // reset the game
-    $(".reset").on("click", function() {
-            location.reload();
-    })
-
-
-    return form;
-
-}
-
-    $(function() {
-        function insertBoxes(products){
-
+    $(function(){
+        function initializeGame(products){
             var api = 'https://api.predic8.de/shop/products/';       
-
             $.ajax({
                 url: api,
                 dataType: 'json',
             }).done(function(res){
-                initialize(res);      
+                getWord(res);
+                setUpGame();      
             }).fail(function(err){
-                initialize();
+                getWord();
                 console.log('Cannot connect with the server');
             });
-            
         }
+        initializeGame();
+    });
+
+    function setUpGame(){
+        $(".triesLeft").text(maxTries);
+        $(".hintsLeft").text(maxHints);
+    }
+
+    function getWord(res){
+        let listWords = res && res.products && res.products.length && res.products.map(prod => prod.name) ||    wordsBackup; 
+        currentWord = listWords[Math.floor(Math.random() * listWords.length)].toUpperCase();
+        console.log(currentWord);
+        createInputs();
+    };
     
-            insertBoxes();
+    function createInputs(){
+        const inputBoxes = $(".inputBoxes");
+        const correctLetters = $(".correctLetters");
+        
+        splitLetters = currentWord.split('');
+        console.log(splitLetters);
+        splitLetters.forEach(function (e,i){
+            if(splitLetters[i] === " "){
+                inputBoxes.append($('<input disabled="true" value=" " id='+i+'>'));
+            } else {
+                inputBoxes.append($('<input id='+i+'>'));
+                correctLetters.append($('<span class="correctGuess" id='+i+'>'));
+            }
+        })
+        allowOnlyLetters();
+    }
 
-    })
+    function allowOnlyLetters(){
+        let inputs = $("input");
+        const regEx = /^[A-Za-z]{1}$/;
+        inputs.on("keyup", function(event){
+            if(!regEx.test($(event.target).val())) {
+                $(event.target).val("");
+            }
+        })
+        verifyInputs();
+    }
+
+    //function verifyInputs(){
+    //    let allInputs = $("input");
+    //    let triesLeft = maxTries;
+    //    let hintsLeft = maxHints;
+//
+    //    allInputs.on("keyup", function(event){
+    //        let i = event.target.id;
+    //        let input = event.target;
+    //        let value = input.value.toUpperCase();
+    //        let correctLetters = $(".correctLetters");
+    //            if(value == splitLetters[i]) {
+    //                //correct
+    //                $(input).addClass('correct');
+    //                $(input).val(value);
+    //                $(correctLetters).children([i]).val(value);
+    //            } else if(splitLetters.indexOf(value) >= 0){
+    //                //almost
+    //                $(input).addClass('almost');
+    //                $(input).val(value);
+    //                triesLeft--;
+    //                setUpGame();
+    //            }  else {
+    //                // incorrect
+    //                $(input).addClass('incorrect');
+    //                $(input).val(value);
+    //                triesLeft--;
+    //                setUpGame()
+    //            } 
+    //    });
+    //}
+
     
+   function verifyInputs() {
+       $("input").on("keyup", function(event) {
+        let input = event.target;
+        let value = input.value.toUpperCase();
+        let i = event.target.id;
+        const correctLetters = $(".correctLetters");
+        //let triesLeft = maxTries;
+        //let hintsLeft = maxHints;      
+        $(input).removeClass();
+        
+            if(value == splitLetters[i]) {
+                $(input).addClass('correct');
+                $(input).val(value);
+                $(correctLetters).children([i]).val(value);
+            } else if(splitLetters.indexOf(value) >= 0){
+                $(input).addClass('almost');
+                $(input).val(value);
+                //triesLeft--;
+            } else {
+                $(input).addClass('incorrect');
+                $(input).val(value);
+                //triesLeft--;           
+            }
+        });
+    };
 
-           
-            
-})
+    function checkWin(){
+        
+    }
 
- 
+});    
